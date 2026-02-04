e72f28722d5ce3b10444234be012d85bf7638285
Fridelhuber <161613371+Fridelhuber@users.noreply.github.com>
2026-02-04 18:39:38 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+const Config = {
+    // Deine echte Server IP
+    SERVER_URL: "http://127.0.0.1:30120/admin_pannel_flo", 
+    
+    // Dein echtes Passwort aus der server.lua
+    API_SECRET: "mein_geheimes_passwort_123" 
+};
